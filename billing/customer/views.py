614d21eeb2290cf00fddb263c1ed185c479f1181
PetroPliuta <peter@splynx.com>
2020-09-12 19:30:55 +0300

    update

--- billing/customer/views.py
+++ billing/customer/views.py
@@ -2,6 +2,7 @@ from .models import Customer
 from rest_framework import viewsets
 from .serializers import CustomerSerializer
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 def index(request):
@@ -11,3 +12,5 @@ def index(request):
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ("login", "password")
--- billing/settings.py
+++ billing/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'billing.customer',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,7 @@ STATIC_ROOT = BASE_DIR / "static-files"
 #         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
 #     ]
 # }
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
