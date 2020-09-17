50e57e8243ad8bd517793716b296abf7822cc96b
PetroPliuta <peter@splynx.com>
2020-09-17 13:48:35 +0300

    update

--- billing/customer/admin.py
+++ billing/customer/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Transaction
 
 
-def get_all_fields(class_):
-    return [key for key in class_.__dict__.keys() if not key.startswith("__")]
+# def get_all_fields(class_):
+#     return [key for key in class_.__dict__.keys() if not key.startswith("__")]
 
 
 class CustomerAdmin(admin.ModelAdmin):
     # list_display = list(get_all_fields(Customer))
-    list_display = ('id', 'login', 'ip_address', 'online')
+    list_display = ('id', 'login', 'ip_address', 'balance', 'active', 'online')
     list_display_links = ('login',)
-    list_editable = ('ip_address',)
-    list_filter = ('online',)
-    readonly_fields = ('online',)
+    list_editable = ('ip_address', 'active')
+    list_filter = ('active', 'online')
+    readonly_fields = ('online', 'balance')
     save_as = True
     save_as_continue = False
     search_fields = ('login', 'ip_address', 'email')
@@ -20,3 +20,13 @@ class CustomerAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Customer, CustomerAdmin)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'amount', 'date_time')
+    list_display_links = ('customer', 'amount')
+    list_filter = ('customer',)
+    ordering = ("id",)
+
+
+admin.site.register(Transaction, TransactionAdmin)
--- billing/customer/models.py
+++ billing/customer/models.py
@@ -1,16 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Customer(models.Model):
     login = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=255, blank=True)
     email = models.EmailField(max_length=255, blank=True)
     ip_address = models.GenericIPAddressField(
         protocol='IPv4', blank=True, null=True)
     mac_address = models.CharField(max_length=255, blank=True)
     added = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
     online = models.BooleanField(default=False)
     # status = enabled, disabled
+    description = models.TextField(blank=True, max_length=10**4)
 
     def __str__(self):
         return self.login
+
+    def balance(self):
+        transactions = Transaction.objects.filter(customer=self)
+        sum = 0
+        for transaction in transactions:
+            sum = sum + transaction.amount
+        return sum
+
+
+class Transaction(models.Model):
+    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date_time = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True, max_length=10**4)
+
+    def __str__(self):
+        return str(self.id) + ' ' + str(self.date_time)
