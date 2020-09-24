38292178d1116137349e388a2711725b2c1172bb
PetroPliuta <peter@splynx.com>
2020-09-24 21:07:47 +0300

    fix

--- billing/customer/serializers.py
+++ billing/customer/serializers.py
@@ -6,5 +6,5 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('id', 'login', 'password', 'ip_address',
-                  'mac_address', 'active', 'online', 'last_online_datetime',
+                  'active', 'online', 'last_online_datetime',
                   'last_online_ip', 'last_online_router', 'balance', 'download_speed', 'upload_speed')
