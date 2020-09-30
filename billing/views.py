247b699648e6d2613078ddb48efee175ce174d02
PetroPliuta <peter@splynx.com>
2020-09-30 23:23:30 +0300

    up

--- billing/views.py
+++ billing/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponseNotFound, HttpResponse
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseForbidden
 import ast
+from billing.customer.models import Customer
 
 
 def index(request):
@@ -12,28 +13,37 @@ def index(request):
 def radius_authorize(request):
     if not request.method == 'POST' or request.META['REMOTE_ADDR'] not in ('', '127.0.0.1'):
         return HttpResponseNotFound()
-    # print("auth request:", request)
-    # print("auth request meta remote addr:", request.META['REMOTE_ADDR'])
-    # print(f"scheme: {request.scheme}")
-    # print(request.POST.dict())
-    # print(request.GET.dict())
-    print(f"request.body: type-'{type(request.body)}': '{request.body}'")
+# NAS
+    from_nas = dict(ast.literal_eval(request.body.decode("UTF-8")))
+    nas_username = ''
+    if not 'User-Name' in from_nas.keys():
+        return HttpResponseForbidden()
+    nas_username = from_nas['User-Name']
+# customer
+    customer = Customer.objects.filter(login=nas_username)
+    if len(customer) != 1:
+        return HttpResponseForbidden()
+    if not customer.active or customer.balance() < 0:
+        return HttpResponseForbidden()
+    radius_reply = {
+        'Acct-Interim-Interval': '60',
+        'Mikrotik-Rate-Limit': f"{customer.upload_speed()}k/{customer.download_speed()}k",
+    }
+    radius_config = {
+        'Cleartext-Password': customer.password,
+    }
 
-    dict_str = request.body.decode("UTF-8")
-    print(f"dict_str: type-'{type(dict_str)}': '{dict_str}'")
+    # dhcp
+    if 'User-Password' in from_nas.keys() and from_nas['User-Password'] == '' and customer.password == '':
+        radius_config['Cleartext-Password'] = 'dhcp'
 
-    mydata = ast.literal_eval(dict_str)
-    print(f"mydata: type-'{type(mydata)}': '{mydata}'")
-
-    # return HttpResponse(request.body)
-    # return HttpResponse(request.POST.lists())
-
-    dict_ = dict(mydata)
-    # return HttpResponse(dict_)
-    return JsonResponse(dict_)
+    if customer.ip_address:
+        radius_reply['Framed-IP-Address'] = customer.ip_address
+    response = {"reply": radius_reply, "config": radius_config}
+    return JsonResponse(response)
 
 
-@csrf_exempt
+@ csrf_exempt
 def radius_accounting(request):
     if not request.method == 'POST' or request.META['REMOTE_ADDR'] not in ('', '127.0.0.1'):
         return HttpResponseNotFound()
