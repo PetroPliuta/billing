f030c66b5b8470f1374056420672f81b9400b2d3
PetroPliuta <peter@splynx.com>
2020-10-02 04:10:05 +0300

    disable coa for dhcp

--- billing/customer/models.py
+++ billing/customer/models.py
@@ -67,10 +67,10 @@ class Customer(models.Model):
 
     def coa(self):
         try:
-            if self.last_online_router:
+            if self.last_online_router and not self.last_online_dhcp:
                 stdin = f"echo 'User-Name=\'{self.last_online_login if self.last_online_login else self.login}\', \
                     Mikrotik-Rate-Limit=\'{self.upload_speed()}k/{self.download_speed()}k\' \
-                    {', Framed-IP-Address='+str(self.last_online_ip) if self.last_online_ip and not self.last_online_dhcp else ''}'"
+                    {', Framed-IP-Address='+str(self.last_online_ip) if self.last_online_ip else ''}'"
                 cmd = f"/usr/bin/env radclient {self.last_online_router.ip_address}:3799 coa \'{self.last_online_router.secret}\' &"
                 subprocess.Popen(stdin+"|"+cmd, shell=True)
         except Exception as ex:
